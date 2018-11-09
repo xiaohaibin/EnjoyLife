3491189a201ef1980457a0ea4d1244d64b99543d
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-11-09 17:30:27 +0800

    优化代码，新增图片加载进度监听

--- app/src/main/java/com/stx/xhb/enjoylife/config/MyAppGlideModule.java
+++ app/src/main/java/com/stx/xhb/enjoylife/config/MyAppGlideModule.java
@@ -5,13 +5,11 @@ import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
 
 import java.io.InputStream;
 
-import me.jessyan.progressmanager.ProgressManager;
 import okhttp3.OkHttpClient;
 
 /**
@@ -27,8 +25,8 @@ public final class MyAppGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(Context context, Glide glide, Registry registry) {
         //Glide 底层默认使用 HttpConnection 进行网络请求,这里替换为 Okhttp 后才能使用本框架,进行 Glide 的加载进度监听
-        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(ProgressManager.getInstance().with(new OkHttpClient.Builder())
-                .build()));
+//        registry.replace(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(ProgressManager.getInstance().with(new OkHttpClient.Builder())
+//                .build()));
     }
 
     @Override
