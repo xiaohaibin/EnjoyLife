2083cbe395bae8af7dace8952523ebc4cf5fe67b
xiaohaibin <xiao.haibin05485@westone.com.cn>
2018-11-09 17:33:14 +0800

    优化代码，新增图片加载进度监听

--- app/src/main/java/com/stx/xhb/enjoylife/config/MyAppGlideModule.java
+++ app/src/main/java/com/stx/xhb/enjoylife/config/MyAppGlideModule.java
@@ -5,12 +5,7 @@ import android.content.Context;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.load.model.GlideUrl;
 import com.bumptech.glide.module.AppGlideModule;
-
-import java.io.InputStream;
-
-import okhttp3.OkHttpClient;
 
 /**
  * @author: xiaohaibin.
